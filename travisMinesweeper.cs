353f7382286b6b2352927ba48f42de302f2d2d9b
pbrizardo <pbrizardo@gmail.com>
2015-10-30 15:35:50 -0700

    Update travisMinesweeper.cs

--- travisMinesweeper.cs
+++ travisMinesweeper.cs
@@ -12,7 +12,6 @@ public void Main()
 	
 	Random rnd = new Random();
 	
-	var boardArr = new string[LENGTH]; // stores board values
 	var arr = new int[LENGTH];         // stores mine count values
 	var count = 0;
 	var row = 0;
@@ -20,13 +19,15 @@ public void Main()
 	for (var i = 0; i < LENGTH; i++) {
 	
 	   // detect of row
+	   var isBeginning = (count == 0);
        var isEndOfLine = (count == MAX_ARR_X);
 		
 	   // randomly generate value
 	   var val = (rnd.Next(1,50)%5 == 1);
 	   board[i] = val;
+		
+	   // print value
        var c = val ? "X" : "O";
-	   boardArr[i] = c;
 	   Console.Write(c);
 		
 	   // print out padding
@@ -37,15 +38,20 @@ public void Main()
 	   // print next line if end of row, and reset count
 	   if (isEndOfLine) Console.Write('\n');
 	   count = (isEndOfLine) ? 0 : count + 1;
-		
+	   
 	   // edge detection valdiation
+	   
 	   var isLeftEdge = (i == row*MAX_X);
 	   var isTopEdge = (i <= MAX_ARR_X);
-	   var isRightEdge = (i == row*MAX_ARR_X);
-	   var isBottomEdge = i >= LENGTH-1 - MAX_X;
+	   var isRightEdge = (i == row*MAX_X + MAX_ARR_X);
+	   var isBottomEdge = (i >= LENGTH-1 - MAX_X);
+	   
 		
 	   if (val) {
-	     // check left
+		   // increm bomb pos
+		   arr[i]++;
+		   
+	       // check left
 		   if (!isLeftEdge) { arr[i-1]++; }
 		   if (!isTopEdge) { arr[i-MAX_X]++; }
 		   if (!isRightEdge) { arr[i+1]++; }
@@ -58,7 +64,8 @@ public void Main()
 		   if (!isRightEdge && !isBottomEdge) { arr[(i+MAX_X+1)]++;}
 	   }
 		
-	   row++;
+	   // increm row count
+	   if (isEndOfLine) { row++; }
 	}	
 	
 	Console.WriteLine("");
